06301509018ce31a7c910bfdbe660c2bc7342b61
陈浩 <13784197113@163.com>
2021-09-30 16:35:35 +0800

    初步提交前端页面，但是有bug

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    # return HttpResponse("欢迎访问我的博客首页")
-
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': "欢迎访问我的博客首页！"
+        'post_list': post_list
     })
 
